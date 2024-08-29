2f594f5e9235034e9c68864916d023dee4145a0c
garrychrstn <garrychrstn@gmail.com>
2024-08-29 13:10:21 +0700

    minor changes

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -5,23 +5,27 @@ const Content = () => {
     const [content, setContent] = useContext(Context);
     const [page, setPage] = useContext(PageContext);
     
+    let y24 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
     let timeline = []
-    const calcMonth = () => {
-        content.forEach(c => {
-            let ni = {
-                id: c.id,
-                position: c.position,
-                company: c.company,
-                start_date: c.start_date.slice(5, 7),
-                end_date: c.end_date.slice(5, 7)
-            }
-            timeline.push(ni)
-        });
-    }
+    content.forEach(c => {
+        let ni = {
+            id: c.id,
+            position: c.position,
+            company: c.company,
+            start_date: c.start_date.slice(5, 7),
+            end_date: c.end_date.slice(5, 7),
+        }
+        timeline.push(ni)
+    })
     let compc = [
         {cid: 'PT. Lingkar Kabel Telekom', tail: 'bg-red-400 px-8 py-5'},
         {cid: 'kitanet.id', tail: 'bg-blue-400 px-8 py-5'}
     ]
+    const getTail = (id) => {
+        let tail = compc.find(tail => tail.cid === id);
+
+    }
     let cstruct = []
     if (page === 'exp') {
         cstruct = [
@@ -103,9 +107,9 @@ const Content = () => {
     console.log(content)
     return ( 
         <div className="content m-auto w-2/5 bg-gray-800 p-5 rounded-lg">
-            { timeline.map((t) => (
-                <div className={ compc[t.id]}></div>
-            ))}
+            { y24.map((m) => {
+                
+            })}
         </div>
      );
 }
